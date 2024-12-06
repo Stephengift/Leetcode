e5ea80fd931a90a19233bbc1d5450beef96e9102
Stephengift <76864404+Stephengift@users.noreply.github.com>
2024-12-05 19:19:18 -0500

    Time: 26 ms (7.12%) | Memory: 44.6 MB (31.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,20 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.toCharArray() == t.toCharArray()){
+        if (s.length() != t.length()){
             return false;
         }
-       HashMap<Character, Integer> hmap1 = new HashMap<>();
-        HashMap<Character, Integer> hmap2 = new HashMap<>();
+        HashMap<Character, Integer> hmap1 = new HashMap <>();
+        HashMap<Character, Integer> hmap2 = new HashMap <>();
 
-        for(Character character: s.toCharArray()){
-            hmap1.put(character, hmap1.getOrDefault(character,0)+1);
+        for(int i = 0; i < s.length();i++){
+            hmap1.put(s.charAt(i), hmap1.getOrDefault(s.charAt(i), 0) + 1);
+            hmap2.put(t.charAt(i), hmap2.getOrDefault(t.charAt(i), 0) + 1);
         }
-        for(Character character: t.toCharArray()){
-            hmap2.put(character, hmap2.getOrDefault(character,0)+1);
+
+        if(hmap1.equals(hmap2)){
+            return true;
         }
-        return hmap1.equals(hmap2);
-    
+        return false;
+
     }
 }
