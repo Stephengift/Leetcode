a635cf27a51a7b6507646b4f4d6e4ff4cd73016b
Stephengift <76864404+Stephengift@users.noreply.github.com>
2024-12-09 11:38:03 -0500

    Time: 6 ms (15.79%) | Memory: 46.5 MB (94.23%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        //following popular twoSum solution, but I am adding index+1 into 
+        //the hashmap to start from index 1 instead of 0
        HashMap<Integer, Integer> hmap = new HashMap <>();
 
        for(int i = 0; i< numbers.length;i++){
