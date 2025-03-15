921f74fd2b3f77f37faca7a6f2ca0ccb26e6899a
Stephengift <76864404+Stephengift@users.noreply.github.com>
2025-03-15 18:46:10 -0400

    Time: 43 ms (5.13%) | Memory: 59 MB (5.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,39 +1,41 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        //error checking
-        HashSet<String> visited = new HashSet<>();
-        if(grid == null || grid.length == 0){
-            return 0;
-        }
-        int numIslands = 0;
-        //going through rows
-        for(int row = 0; row < grid.length; row++){
-            for(int column = 0; column < grid[row].length; column++){
-                if(dfs(grid, row,column, visited) == true){
-                    numIslands++;
+	public int numIslands(char[][] grid){
+		HashSet<String> set = new HashSet<>();
+		int islands = 0;
+		for(int row = 0; row  < grid.length;row++){
+			for(int column = 0; column < grid[row].length; column++){
+				if(dfs(row,column,grid,set)){
+					islands++;
                 }
             }
         }
-        return numIslands;
+		return islands;
     }
+		
 
-    public boolean dfs (char[][] grid, int row, int column, HashSet<String> visited){
-        //out of bounds
-        if(row < 0 || row >= grid.length || column < 0 || column >= grid[row].length || grid[row][column] == '0'){
-            return false;
-        }
-        if(visited.contains(row + "," + column)){
-            return false;
-        }
-        visited.add(row + "," + column);
-        dfs(grid, row+ 1, column, visited);
-        dfs(grid, row- 1, column, visited);
-        dfs(grid, row, column + 1, visited);
-        dfs(grid, row, column - 1, visited);
 
-        return true;
+
+public boolean dfs(int row, int column, char[][] grid, HashSet<String> set){
+    String stringKey = row + "," + column;
+        if( row < 0 || column < 0 || row >= grid.length || column >= grid[row].length || grid[row][column] == '0'){
+                return false;
+        }
+
     
+    if(set.contains(stringKey)){
+        return false;
     }
+    set.add(stringKey);
+
+    dfs(row+1, column, grid, set);
+    dfs(row-1, column, grid, set);
+    dfs(row, column+1, grid, set);
+    dfs(row, column-1, grid, set);
+
+
+    return true;
+    }
+
     
     
 }
